27cd46be37e23fdf46ed02001144870e44674fcd
Arturo Balbi <balbiarturo@gmail.com>
2023-05-21 16:13:17 -0300

    Cambio de likns de refernecia

--- js/contacto.js
+++ js/contacto.js
@@ -37,13 +37,13 @@ const validarCampo = (expresion, input, campo) => {
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
 		document.querySelector(`#grupo__${campo} i`).classList.add('fa-circle-check');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-circle-check');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-circle-xmark');
 		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
         campos[campo] = true;
 	} else {
 		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-circle-check');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-circle-xmark');
 		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-circle-check');
 		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
         campos[campo] = false;
